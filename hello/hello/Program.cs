31a476b13c61f0bc69de17deb9ac1cf9069fbd03
Benjamin M. Brown <benjaminbb@gmail.com>
2015-10-15 23:02:22 -0500

    Initial commit: Simple hello world and did an overview of basic c# debugging with breakpoints, setting debug start parameters, exploring solution properties

---
+++ hello/hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // if (args[0]) { string name = args[0]; }
+            string name = args[0];
+            Console.WriteLine("Hey, World. Sup "+ name);
+        }
+    }
+}
