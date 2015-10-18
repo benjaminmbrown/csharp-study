6107f15673ac17921b18a2e731054bbf8cd3aed9
Benjamin M. Brown <benjaminbb@gmail.com>
2015-10-18 18:02:24 -0500

    Simple program ... real simple

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -10,9 +10,25 @@ namespace hello
     {
         static void Main(string[] args)
         {
-           // if (args[0]) { string name = args[0]; }
-            string name = args[0];
-            Console.WriteLine("Hey, World. Sup "+ name);
+            // if (args[0]) { string name = args[0]; }
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How much sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hey, World. Sup " + name);
+
+            if (hoursOfSleep < 8)
+            {
+                Console.WriteLine("You must be tired!");
+            }
+            else
+            {
+                Console.WriteLine("I bet you feel rested!");
+            }
+
+            
         }
     }
 }
