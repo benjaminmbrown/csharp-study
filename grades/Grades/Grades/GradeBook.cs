d7cb36e3b1862ccee864e78c41c31f86a80553de
Benjamin M. Brown <benjaminbb@gmail.com>
2015-10-20 21:22:29 -0500

    First multiclass project setup & going over class definitions

---
+++ grades/Grades/Grades/GradeBook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {   
+        //Class members can be either:
+        //1 . State - data to work with inside class (grades here)
+        //grades user entered
+
+        //2. behavior members - do work (methods)
+           // a. Add a new grade
+           // b. Calculate stats
+
+        public void AddGrade(float grade)
+        {
+            //Double and float types
+            //Float - 7 digit precision
+            //Double - 14 digit precision
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+
+    }
+}
