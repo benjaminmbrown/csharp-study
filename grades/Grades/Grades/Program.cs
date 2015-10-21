c28b39ea53b629c03401f0d8795e7fbf4153ccfb
Benjamin M. Brown <benjaminbb@gmail.com>
2015-10-20 21:42:43 -0500

    Exposing difference between classes, objects, and variables

--- grades/Grades/Grades/Program.cs
+++ grades/Grades/Grades/Program.cs
@@ -13,6 +13,9 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(92);
             book.AddGrade(34);
+
+            GradeBook book2 = new GradeBook();
+            book2.AddGrade(75);
         }
     }
 }
