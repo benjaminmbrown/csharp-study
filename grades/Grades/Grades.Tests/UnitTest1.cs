0c3c6514d73d659307b3124c5e2f4eefc2bcf429
unknown <brownbe@ITN-BROWNBE.us.pressganey.com>
2015-10-22 17:50:20 -0500

    Unit tests - adding unit tests and understanding adding references to projects for testing purposes

---
+++ grades/Grades/Grades.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void CalculatesHighestGrade()
+        {
+            bool value = true;
+            GradeBook book = new GradeBook();
+
+
+            book.AddGrade(90f);
+            book.AddGrade(33f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Assert.AreEqual(90f, stats.HighestGrade);
+        }
+    }
+}
